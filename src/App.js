fa10e08a6f4361475fc58856cf0004eeb6d9576d
Agatino-P <57068089+Agatino-P@users.noreply.github.com>
2021-08-15 11:40:18 +0200

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="help">
+          Pick 1 or more numbers that sum 
+          to the number of stars
+        </div>
+        <div className="body">
+          <div className="stars">
+            <div className="star" />
+            <div className="star" />
+            <div className="star" />
+            <div className="star" />
+            <div className="star" />
+            <div className="star" />
+            <div className="star" />
+            <div className="star" />
+            <div className="star" />
+          </div>
+          <div className="play-numbers">
+            <button className="number">1</button>
+            <button className="number">2</button>
+            <button className="number">3</button>
+            <button className="number">4</button>
+            <button className="number">5</button>
+            <button className="number">6</button>
+            <button className="number">7</button>
+            <button className="number">8</button>
+            <button className="number">9</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
+
 
 export default App;
